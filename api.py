568982af0c1d3ba0e7d9ecec8bc6dfa19e86fd81
Aleena Malik <aleenamalik1255@gmail.com>
2025-12-06 19:00:58 +0500

    Update Flask to use Render PORT

--- api.py
+++ api.py
@@ -60,6 +60,9 @@ def recommend():
 # -----------------------------
 # Run the app
 # -----------------------------
+import os
+
 if __name__ == "__main__":
-    # Development server:
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's PORT if available
+    app.run(host="0.0.0.0", port=port)
+
